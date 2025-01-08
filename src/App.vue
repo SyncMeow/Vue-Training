6bf6a22f7fd637a00b9f35d7bb36db4a99bc7aa6
SyncMeow <firelatergun1209@gmail.com>
2025-01-08 14:19:28 +0800

    input practice

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,40 @@
 <script setup>
-    import {ref} from "vue";
+import { ref } from "vue";
 
-    let className = ref("plainText");
+let className = ref("plainText");
 
-    let flag = ref(0);
+let flag = ref(0);
 
-    let handler = function() {
-        flag.value = flag.value? 0: 1;
-        className.value = className.value === "plainText"? "title highlight": "plainText";
-    };
+let handler = function () {
+    flag.value = flag.value ? 0 : 1;
+    className.value =
+        className.value === "plainText" ? "title highlight" : "plainText";
+};
 
-    let content = ["There is a secret message behind this text", "Congratulations You found it"];
+let content = [
+    "There is a secret message behind this text",
+    "Congratulations You found it",
+];
+
+let gender = ref(null);
+
+let formatter = function(value){
+    if (value == null) return "";
+
+    let res = "";
+    for (let i = 0; i < value.length; i++) {
+        res += (!i)? value[i].toUpperCase() : value[i].toLowerCase();
+    }
+    return res;
+};
+
 </script>
 
 <template>
     <main>
         <h1>Home</h1>
+
+        <!--
 
         <div><input type = "text" /></div>
         <div><textarea> Write something in it. </textarea></div>
@@ -32,16 +51,32 @@
             </select>
         </div>
 
-        <div :class = "className">{{ content[flag] }}</div>
+        -->
+
+        Male <input type="radio" value="male" v-model="gender" />
+        Female <input type="radio" value="female" v-model="gender" />
+        <div> Your gender is {{ formatter(gender) }} </div>
+
+        <div :class="className">{{ content[flag] }}</div>
 
         <button @click="handler">Click me</button>
     </main>
 </template>
 
 <style scoped>
-    main{background-color: white;}
-    .title{font-weight: bold;}
-    .highlight{color: red;}
-    .content{color: blue;}
-    
+main {
+    background-color: white;
+}
+
+.title {
+    font-weight: bold;
+}
+
+.highlight {
+    color: red;
+}
+
+.content {
+    color: blue;
+}
 </style>
